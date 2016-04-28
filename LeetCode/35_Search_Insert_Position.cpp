e63fdbb587bc375ba30486a9141510ed658b5857
Rainbowbox <twentyfivehours@163.com>
2016-04-28 23:54:42 +0800

    AC 35 Search insert position

--- LeetCode/35_Search_Insert_Position.cpp
+++ LeetCode/35_Search_Insert_Position.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <vector>
+#include "gtest/gtest.h"
+
 using namespace std;
 
 /*
@@ -16,9 +18,49 @@ using namespace std;
 
 namespace LeetCode {
     class Solution {
+    private:
+        int _size;
     public:
         int searchInsert(vector<int>& nums, int target) {
-
+            _size = (int)nums.size();
+            return search(nums, 0, _size - 1, target);
+        }
+    private:
+        int search(vector<int>&nums, int low, int high, int target){
+            if(low > high){
+                if(low > _size)
+                    return _size;
+                if(high < 0)
+                    return 0;
+                if(nums[high] < target)
+                    return high + 1;
+                if(nums[low] > target)
+                    return low - 1;
+            }
+            int mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            
+            if(nums[mid] < target)
+                return search(nums, mid + 1, high, target);
+            
+            return search(nums, low, mid - 1, target);
         }
     };
+    
+    class helper{
+    public:
+        void test(vector<int>nums, int target, int expected){
+            Solution sln;
+            int actual = sln.searchInsert(nums, target);
+            ASSERT_EQ(expected, actual);
+        }
+    };
+    TEST(SearchInsertPosition, searchInsert){
+        helper h;
+        h.test({1, 3, 5, 6}, 5, 2);
+        h.test({1, 3, 5, 6}, 2, 1);
+        h.test({1, 3, 5, 6}, 7, 4);
+        h.test({1, 3, 5, 6}, 0, 0);
+    }
 }
