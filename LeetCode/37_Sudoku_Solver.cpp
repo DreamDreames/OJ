c936841fe4b3bd907b5d2fb3b3a799f007bb9f58
Rainbowbox <twentyfivehours@163.com>
2016-04-29 00:58:54 +0800

    Add 37 Sudoku solver

---
+++ LeetCode/37_Sudoku_Solver.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+/*
+ Write a program to solve a Sudoku puzzle by filling the empty cells.
+ 
+ Empty cells are indicated by the character '.'.
+ 
+ You may assume that there will be only one unique solution.
+*/
+
+namespace LeetCode {
+    class Solution {
+    public:
+        void solveSudoku(vector<vector<char>>& board) {
+            
+        }
+    };
+}
