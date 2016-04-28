c5319e7db13cfd9307c2923a4a8842143f88108c
Rainbowbox <twentyfivehours@163.com>
2016-04-28 23:30:37 +0800

    AC 34 Search for a range

--- LeetCode/34_Search_for_a_Range.cpp
+++ LeetCode/34_Search_for_a_Range.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <stdio.h>
 #include <vector>
 #include "gtest/gtest.h"
@@ -22,7 +21,49 @@ namespace LeetCode {
     public:
         vector<int> searchRange(vector<int>& nums, int target) {
             vector<int> ans;
+            int len = (int)nums.size();
+            int l = searchLeft(nums, 0, len - 1, target);
+            int r = searchRight(nums, 0, len -1, target);
+            ans.push_back(l);
+            ans.push_back(r);
             return ans;
+        }
+        int searchLeft(vector<int>& nums, int low, int high, int target){
+            if(low > high)
+                return -1;
+            
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                if(mid > 0 && nums[mid - 1] < target)
+                    return mid;
+                if(mid == 0)
+                    return 0;
+                return searchLeft(nums, low, mid - 1, target);
+            }
+            if(nums[mid] < target){
+                return searchLeft(nums, mid + 1, high, target);
+            }
+            return searchLeft(nums, low, mid - 1, target);
+        }
+        
+        int searchRight(vector<int>& nums, int low, int high, int target){
+            if(low > high)
+                return -1;
+            
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                if(mid < high && nums[mid + 1] > target){
+                    return mid;
+                }
+                if(mid == high){
+                    return high;
+                }
+                return searchRight(nums, mid + 1, high, target);
+            }
+            if(nums[mid] > target)
+                return searchRight(nums, low, mid - 1, target);
+            
+            return searchRight(nums, mid + 1, high, target);
         }
     };
     
@@ -38,5 +79,9 @@ namespace LeetCode {
     TEST(SEARCH_for_a_RANGE, searchRange){
         helper h;
         h.test({5, 7, 7, 8, 8, 10}, 8, {3, 4});
+        h.test({5, 7, 7, 8, 8, 10}, 9, {-1, -1});
+        h.test({5, 7, 7, 8, 8, 10}, 5, {0, 0});
+        h.test({5, 7, 7, 8, 8, 10}, 10, {5, 5});
+        h.test({5, 6, 7, 7, 8, 8, 10}, 6, {1, 1});
     }
 }
