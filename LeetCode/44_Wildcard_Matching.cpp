9c9e57aaa23e0176706209fd43d1cad907d77a77
Rainbowbox <twentyfivehours@163.com>
2016-05-03 17:21:24 +0800

    44 Wildcard matching TLE

--- LeetCode/44_Wildcard_Matching.cpp
+++ LeetCode/44_Wildcard_Matching.cpp
@@ -1,6 +1,7 @@
+#include "stdafx.h"
 #include <stdio.h>
 #include <string>
-#include "gtest/gtest.h"
+//#include "gtest/gtest.h"
 
 using namespace std;
 
@@ -29,8 +30,48 @@ namespace LeetCode {
     class WildCardMatchingSolution {
     public:
         bool isMatch(string s, string p) {
-            return false;
+			_slen = s.length();
+			_plen = p.length();
+			for (int i = 0; i < _slen + 2; ++ i) {
+				vector<int> temp(_plen + 2, -1);
+				_status.push_back(temp);
+			}
+			return isMatch(s, 0, p, 0);
         }
+	private:
+		bool isMatch(string s, int si, string p, int pi){
+			if (_status[si][pi] >= 0) {
+				return _status[si][pi] == 1;
+			}
+			bool result;
+			if (si == _slen){
+				if (pi == _plen) {
+					result = true;
+				}
+				else if (p[pi] == '*')
+					result = isMatch(s, si, p, pi + 1) == 1;
+				else
+					result = false;
+			}
+			else if (pi == _plen){
+				result = si == _slen;
+			}
+			else if (p[pi] == '?'){
+				result = isMatch(s, si + 1, p, pi + 1);
+			}
+			else if (p[pi] == '*'){
+				result = isMatch(s, si + 1, p, pi + 1) || isMatch(s, si + 1, p, pi);
+			}
+			else{
+				result = s[si] == p[pi] && isMatch(s, si + 1, p, pi + 1);
+			}
+			_status[si][pi] = result ? 1 : 0;
+			return result;
+		}
+	private:
+		int _slen;
+		int _plen;
+		vector<vector<int>> _status;
     };
     
     class wdmhelper{
@@ -38,18 +79,22 @@ namespace LeetCode {
         void test(string s, string p, bool expected){
             WildCardMatchingSolution sln;
             bool actual = sln.isMatch(s, p);
-            ASSERT_EQ(expected, actual);
+			Assert::AreEqual(expected, actual);
         }
         
     };
-    TEST(WildcardMaching, isMatch){
-        wdmhelper h;
-        h.test("aa", "a", false);
-        h.test("aa", "aa", true);
-        h.test("aaa", "aa", false);
-        h.test("aa", "*", true);
-        h.test("aa", "a*", true);
-        h.test("ab", "?*", true);
-        h.test("aab", "c*a*b", false);
-    }
+	TEST_CLASS(WildcardMatching) {
+	public:
+		TEST_METHOD(WildCardMatchingTest) {
+			wdmhelper h;
+			h.test("aa", "a", false);
+			h.test("aa", "aa", true);
+			h.test("aaa", "aa", false);
+			h.test("aa", "*", true);
+			h.test("aa", "a*", true);
+			h.test("ab", "?*", true);
+			h.test("aab", "c*a*b", false);
+			h.test("aa", "aa*", true);
+		}
+	};
 }
