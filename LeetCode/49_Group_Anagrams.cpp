1b4b236444f033fe12a6d3ea457ff454b05fa9a7
Rainbowbox <twentyfivehours@163.com>
2016-05-09 15:58:12 +0800

    Add 49 Group Anagrams

---
+++ LeetCode/49_Group_Anagrams.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <string>
+#include <vector>
+
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Return:
+
+[
+["ate", "eat","tea"],
+["nat","tan"],
+["bat"]
+]
+Note:
+For the return value, each inner list's elements must follow the lexicographic order.
+All inputs will be in lower-case.
+*/
+namespace GroupAnagrams {
+	class Solution {
+	public:
+		vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+		}
+	};
+}
