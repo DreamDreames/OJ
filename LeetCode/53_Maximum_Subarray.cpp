781ff7d64dbcf8f1ef6dd2c5ec4a5fd527dc1826
Rainbowbox <twentyfivehours@163.com>
2016-05-09 21:44:43 +0800

    AC 53 Maximum Subarray

---
+++ LeetCode/53_Maximum_Subarray.cpp
@@ -0,0 +1,61 @@
+#include "shared.h"
+#include <stdio.h>
+
+/*
+ Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ 
+ For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+ the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+ 
+ click to show more practice.
+ 
+ More practice:
+ If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+namespace MaximumSubarray {
+    class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int lastPostive = -1, currentSum = 0;
+            int max = nums[0];
+            for(int i = 1; i < nums.size(); ++ i){
+                if(max < nums[i])
+                    max = nums[i];
+            }
+            
+            for(int i = 0; i < nums.size(); ++ i){
+                if(nums[i] > 0){
+                    if(currentSum >= 0)
+                        currentSum += nums[i];
+                    else{
+                        currentSum = nums[i];
+                    }
+                    if(max < currentSum){
+                        max = currentSum;
+                    }
+                    lastPostive = i;
+                }
+                else{
+                    currentSum += nums[i];
+                }
+            }
+            return max;
+        }
+    };
+    class helper{
+    public:
+        void test(vector<int> nums, int expected){
+            Solution sln;
+            int actual = sln.maxSubArray(nums);
+            ASSERT_EQ(expected, actual);
+        }
+    };
+    TEST(MaximumSubarray, maxSubArray){
+        helper h;
+        h.test({-2,1,-3,4,-1,2,1,-5,4}, 6);
+        h.test({4,-1,2,1}, 6);
+        h.test({4}, 4);
+        h.test({-1, -2, -3, 0}, 0);
+    }
+}
---
+++ shared.h
@@ -0,0 +1,17 @@
+//
+//  shared.h
+//  OJ
+//
+
+#ifndef shared_h
+#define shared_h
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+#endif /* shared_h */
