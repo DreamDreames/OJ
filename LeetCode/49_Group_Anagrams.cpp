ed5452ef7a16133b17022a23f48021ebe09985b0
Yong Chao BIAN <ybian@slb.com>
2016-05-09 16:37:36 +0800

    AC 49 Group Anagrams

--- LeetCode/49_Group_Anagrams.cpp
+++ LeetCode/49_Group_Anagrams.cpp
@@ -1,7 +1,11 @@
 #include "stdafx.h"
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 
+using namespace std;
 /*
 Given an array of strings, group anagrams together.
 
@@ -21,7 +25,41 @@ namespace GroupAnagrams {
 	class Solution {
 	public:
 		vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+			map<string, multiset<string>> collection;
+			for (auto& str : strs) {
+				string copy = str;
+				sort(copy.begin(), copy.end());
+				auto iter = collection.find(copy);
+				if (iter == collection.end()) {
+					multiset<string> temp{ str };
+					collection[copy] = temp;
+				}
+				else {
+					iter->second.insert(str);
+				}
+			}
+			vector<vector<string>> ans;
+			for (auto &iter : collection) {
+				auto & result = iter.second;
+				vector<string> temp;
+				for (auto& str : result) {
+					temp.push_back(move(str));
+				}
+				ans.push_back(move(temp));
+			}
+			return ans;
+		}
+	};
+	TEST_CLASS(Group) {
+	public:
+		TEST_METHOD(GroupAnagrams) {
+			test({ "eat", "tea", "tan", "ate", "nat", "bat" }, { {"ate", "eat","tea"}, {"nat","tan"}, {"bat"} });
+			test({ "", "" }, { {"", ""} });
+		}
+	private:
+		void test(vector<string> strs, vector<vector<string>> expected) {
+			Solution sln;
+			auto& ans = sln.groupAnagrams(strs);
 		}
 	};
 }
