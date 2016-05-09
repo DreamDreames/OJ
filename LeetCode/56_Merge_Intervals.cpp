c29303b2068f1178914946f3f912fe6b72efabf7
Rainbowbox <twentyfivehours@163.com>
2016-05-09 23:05:48 +0800

    Add 56 Merge Intervals

---
+++ LeetCode/56_Merge_Intervals.cpp
@@ -0,0 +1,27 @@
+#include "shared.h"
+#include <stdio.h>
+
+/*
+ Given a collection of intervals, merge all overlapping intervals.
+ 
+ For example,
+ Given [1,3],[2,6],[8,10],[15,18],
+ return [1,6],[8,10],[15,18].
+*/
+
+namespace MergeIntervals{
+    struct Interval {
+      int start;
+      int end;
+      Interval() : start(0), end(0) {}
+      Interval(int s, int e) : start(s), end(e) {}
+    };
+
+    class Solution {
+    public:
+        vector<Interval> merge(vector<Interval>& intervals) {
+            vector<Interval> ans;
+            return ans;
+        }
+    };
+}
