9c113c8f9896c8050843c86efc5bf41843ad6729
Rainbowbox <twentyfivehours@163.com>
2016-05-09 21:16:04 +0800

    Optimize code

--- LeetCode/51_N_Queens.cpp
+++ LeetCode/51_N_Queens.cpp
@@ -32,43 +32,40 @@ namespace NQueens {
     public:
         vector<vector<string>> solveNQueens(int n) {
             vector<vector<string>> ans;
-            vector<string> status(n, string(n, '.'));
             vector<pair<int,int>> occupied;
-            solve(status, occupied, n, 0, ans);
+            solve(occupied, n, 0, ans);
             return ans;
         }
     private:
-        void solve(vector<string>& status, vector<pair<int,int>>& occupied, int n, int row, vector<vector<string>>& ans){
+        void solve(vector<pair<int,int>>& occupied, int n, int row, vector<vector<string>>& ans){
             for(int i = 0; i < n; ++ i){
-                if(!conflict(status, occupied, row, i)){
-                    status[row][i] = 'Q';
+                if(!conflict(occupied, row, i)){
                     occupied.push_back(make_pair(row, i));
                     if(row == n - 1){
-                        ans.push_back(status);
-                        status[row][i] = '.';
+                        ans.push_back(move(createMatrix(n, occupied)));
                         occupied.pop_back();
                         return;
                     }
-                    solve(status, occupied, n, row + 1, ans);
-                    status[row][i] = '.';
+                    solve(occupied, n, row + 1, ans);
                     occupied.pop_back();
                 }
             }
         }
-        bool conflict(vector<string>& status, vector<pair<int, int>>&occupied, int x, int y){
-            for(int i = 0; i <= x; ++ i){
-                if(status[i][y] == 'Q')
-                    return true;
-            }
-            for(int j = 0; j <= y; ++ j){
-                if(status[x][j] == 'Q')
-                    return true;
-            }
+        bool conflict(vector<pair<int, int>>&occupied, int x, int y){
             for(auto &pos : occupied){
                 if(abs(pos.first - x) == abs(pos.second - y))
                     return true;
+                if(pos.first == x || pos.second == y)
+                    return true;
             }
             return false;
+        }
+        vector<string> createMatrix(int n, vector<pair<int,int>>& occupied){
+            vector<string> result(n, string(n, '.'));
+            for(auto& pos: occupied){
+                result[pos.first][pos.second] = 'Q';
+            }
+            return result;
         }
     };
     class helper{
@@ -85,6 +82,5 @@ namespace NQueens {
         h.test(2, 0);
         h.test(4, 2);
         h.test(8, 92);
-        
     }
 }
