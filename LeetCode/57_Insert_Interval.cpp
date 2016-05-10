44fb1ac7d175b773bb850ddff80baa73fe04f022
Yong Chao BIAN <ybian@slb.com>
2016-05-10 11:14:27 +0800

    AC 57 Insert Interval

--- LeetCode/57_Insert_Interval.cpp
+++ LeetCode/57_Insert_Interval.cpp
@@ -23,7 +23,59 @@ namespace InsertInterval {
 	class Solution {
 	public:
 		vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+			vector<Interval> ans;
+			int i; bool inserted = false;
+			for (i = 0; i < (int)intervals.size(); ++i) {
+				if (newInterval.end < intervals[i].start) {
+					ans.push_back(newInterval);
+					inserted = true;
+					break;
+				}
+				if (intervals[i].end >= newInterval.start) {
+					intervals[i].start = min(intervals[i].start, newInterval.start);
+					intervals[i].end = max(intervals[i].end, newInterval.end);
+					inserted = true;
+					while (i < (int)intervals.size() - 1) {
+						if (intervals[i].end >= intervals[i + 1].start) {
+							intervals[i + 1].start = min(intervals[i].start, intervals[i + 1].start);
+							intervals[i + 1].end = max(intervals[i].end, intervals[i + 1].end);
+						}
+						else
+							ans.push_back(intervals[i]);
+						i++;
+					}
+					break;
+				}
+				ans.push_back(intervals[i]);
+			}
+			while (i < intervals.size()) {
+				ans.push_back(intervals[i++]);
+			}
+			if (!inserted)
+				ans.push_back(newInterval);
 
+			return ans;
+		}
+	};
+
+	TEST_CLASS(insert) {
+	public:
+		TEST_METHOD(InsertInterval) {
+			test({ { 1, 3 },{ 6, 9 } }, { 2, 5 }, { { 1, 5 }, {6, 9}});
+			test({ { 1, 2 },{ 3, 5 }, {6, 7}, {8, 10},{12, 16} }, { 4, 9 }, { { 1, 2 }, {3 ,10}, {12, 16} });
+			test({}, { 1, 2 }, { {1, 2} });
+			test({ {3, 4} }, { 1, 2 }, { {1, 2},{3, 4} });
+			test({ {1, 2} }, { 3, 4 }, { {1, 2},{3, 4} });
+		}
+	private:
+		void test(vector<Interval>intervals, Interval newInterval, vector<Interval> expected) {
+			Solution sln;
+			auto& ans = sln.insert(intervals, newInterval);
+			Assert::AreEqual(expected.size(), ans.size());
+			for (int i = 0; i < expected.size(); ++i) {
+				Assert::AreEqual(expected[i].start, ans[i].start);
+				Assert::AreEqual(expected[i].end, ans[i].end);
+			}
 		}
 	};
 }
