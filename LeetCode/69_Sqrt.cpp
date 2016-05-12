6b12100b340f19718444d1d23bd2f07b591fce32
Rainbowbox <twentyfivehours@163.com>
2016-05-13 00:17:10 +0800

    Add new problems

---
+++ LeetCode/69_Sqrt.cpp
@@ -0,0 +1,16 @@
+#include "shared.h"
+
+/*
+ Implement int sqrt(int x).
+ 
+ Compute and return the square root of x.
+*/
+
+namespace Sqrt{
+    class Solution {
+    public:
+        int mySqrt(int x) {
+            return 0;
+        }
+    };
+}
