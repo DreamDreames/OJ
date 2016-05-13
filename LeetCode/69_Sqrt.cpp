5ec9f0a9157cbfd4b491d82dfd3a7b01059c28a9
Rainbowbox <twentyfivehours@163.com>
2016-05-13 12:55:57 +0800

    AC 69 Sqrt

--- LeetCode/69_Sqrt.cpp
+++ LeetCode/69_Sqrt.cpp
@@ -1,4 +1,4 @@
-#include "shared.h"
+#include "stdafx.h"
 
 /*
  Implement int sqrt(int x).
@@ -10,7 +10,26 @@ namespace Sqrt{
     class Solution {
     public:
         int mySqrt(int x) {
-            return 0;
+			if (x <= 1)
+				return x;
+			return iter(x, 2.0);
         }
+	private:
+		double iter(double x, double guess) {
+			if (abs(guess * guess - x) < 0.0001)
+				return guess;
+
+			return iter(x, (guess + x / guess) / 2);
+		}
     };
+	TEST_CLASS(sqrt) {
+	public:
+		TEST_METHOD(Sqrt) {
+			Solution sln;
+			Assert::AreEqual(2, sln.mySqrt(4));
+			Assert::AreEqual(1, sln.mySqrt(1));
+			Assert::AreEqual(3, sln.mySqrt(9));
+			Assert::AreEqual(1, sln.mySqrt(2));
+		}
+	};
 }
