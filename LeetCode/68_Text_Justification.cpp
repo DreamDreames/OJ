2d6ef88f60813fce45a226805e8e4d281c3ba858
Yong Chao BIAN <ybian@slb.com>
2016-05-13 12:55:57 +0800

    AC 69 Sqrt

--- LeetCode/68_Text_Justification.cpp
+++ LeetCode/68_Text_Justification.cpp
@@ -1,4 +1,4 @@
-#include "shared.h"
+#include "stdafx.h"
 /*
  Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
  
