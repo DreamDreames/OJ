5e734645a314195fd80230d36c606474b6e83e65
Rainbowbox <twentyfivehours@163.com>
2016-05-16 00:10:25 +0800

    Add Merge Sort

--- Algorithms/Sort.cpp
+++ Algorithms/Sort.cpp
@@ -1,7 +1,7 @@
 #include "shared.h"
 
 namespace Algorithms {
-    class Sort{
+    class InnerSort{
     public:
         void insertSort(vector<int>& arr){
             for(int i = 1; i < arr.size(); ++ i){
@@ -26,16 +26,53 @@ namespace Algorithms {
             }
         }
     };
+    class MergeSort{
+    public:
+        void sort(vector<int>& arr){
+            mergeSort(arr, 0, (int)arr.size() - 1);
+        }
+    private:
+        void mergeSort(vector<int>& arr, int p, int r){
+            if (p >= r)
+                return;
+            int q = (p + r) / 2;
+            mergeSort(arr, p, q);
+            mergeSort(arr, q + 1, r);
+            merge(arr, p, q, r);
+        }
+        void merge(vector<int>& arr, int p, int q, int r){
+            vector<int> left (arr.begin() + p, arr.begin() + q + 1);
+            vector<int> right (arr.begin() + q + 1, arr.begin() + r + 1);
+            auto liter = left.begin(), riter = right.begin(), aiter = arr.begin() + p;
+            while(liter != left.end() && riter != right.end()){
+                if(*liter <= *riter)
+                    *(aiter ++) = *(liter ++);
+                else
+                    *(aiter ++) = *(riter ++);
+            }
+            while(liter != left.end()){
+                *(aiter ++) = *(liter ++);
+            }
+            while(riter != right.end()){
+                *(aiter ++) = *(riter ++ );
+            }
+        }
+    };
     class helper{
     public:
         void test(vector<int> original, vector<int> expected){
-            Sort s;
+            InnerSort is;
             auto temp = original;
-            s.insertSort(temp);
+            is.insertSort(temp);
             ASSERT_EQ(temp, expected);
             auto temp2 = original;
-            s.insertSort(&temp2[0], (int)temp2.size());
+            is.insertSort(&temp2[0], (int)temp2.size());
             ASSERT_EQ(temp2, expected);
+            
+            auto temp3 = original;
+            MergeSort ms;
+            ms.sort(temp3);
+            ASSERT_EQ(expected, temp3);
         }
     };
     TEST(Sort, sort) {
