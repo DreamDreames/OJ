a64ad2c87a5db3866baab59d951c60695c7d075b
Rainbowbox <twentyfivehours@163.com>
2016-05-17 00:51:14 +0800

    AC 77 Combinations

--- LeetCode/77_Combinations.cpp
+++ LeetCode/77_Combinations.cpp
@@ -19,7 +19,40 @@ namespace Combinations {
     class Solution {
     public:
         vector<vector<int>> combine(int n, int k) {
-            
+            vector<vector<int>> ans;
+            vector<int> result;
+            iter(1, n, k, result, ans);
+            return ans;
+        }
+    private:
+        void iter(int start, int end, int k, vector<int>& result, vector<vector<int>>& ans){
+            if(k == 0){
+                ans.push_back(result);
+                return;
+            }
+            for(int i = start; i <= end - k + 1; ++ i){
+                result.push_back(i);
+                iter(i + 1, end, k - 1, result, ans);
+                result.pop_back();
+            }
         }
     };
+    class helper{
+    public:
+        void test(int n, int k){
+            Solution sln;
+            auto actual = sln.combine(n, k);
+            for(auto &row: actual){
+                for(auto n: row){
+                    cout << n << " " ;
+                }
+                cout << endl;
+            }
+        }
+        
+    };
+    TEST(Combinations, combine){
+        helper h;
+        h.test(4, 2);
+    }
 }
