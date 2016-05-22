3a425ed805f7f4d50438a0bf09102f6b186bdb60
Rainbowbox <twentyfivehours@163.com>
2016-05-22 15:41:23 +0800

    Update with a better solution

--- LeetCode/42_Trapping_Rain_Water.cpp
+++ LeetCode/42_Trapping_Rain_Water.cpp
@@ -14,10 +14,36 @@ using namespace std;
  
  The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
 */
+
+/*
+ 对任意位置i，在i上的积水，由左右两边最高的bar：A[left] = max{A[j], j<i}, A[right] = max{A[j], j>i}决定。定义Hmin = min(A[left], A[right])，则积水量Si为：
+ 
+ Hmin <= A[i]时，Si = 0
+ Hmin > A[i]时，Si = Hmin - A[i]
+*/
 namespace TrappingRainWater {
     class Solution {
     public:
-        int trap(vector<int>& height) {
+        int trap(vector<int>& height){
+            int size = (int) height.size();
+            vector<int> leftHeight(size, 0);
+            vector<int> rightHeight(size, 0);
+            int water = 0;
+            for(int i = 1; i < size; ++ i){
+                leftHeight[i] = max(leftHeight[i - 1], height[i - 1]);
+            }
+            for(int i = size - 2; i >= 0; -- i){
+                rightHeight[i] = max(rightHeight[i + 1], height[i + 1]);
+                
+                int minHeight = min(leftHeight[i], rightHeight[i]);
+                if(minHeight > height[i])
+                    water += minHeight - height[i];
+            }
+            return water;
+                
+        }
+    private:
+        int trap1(vector<int>& height) {
             int maxWater = 0;
             for(int i = 1; i < (int)height.size() - 1; ++ i){
                 if(!(height[i] <= height[i - 1] && height[i] <= height[i + 1]))
