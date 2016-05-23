0632dcc76df0c116752bbcc01ce61f8ac1daef3a
Rainbowbox <twentyfivehours@163.com>
2016-05-23 23:41:59 +0800

    Add 90 Subset 2

---
+++ LeetCode/90_Subset2.cpp
@@ -0,0 +1,28 @@
+#include "shared.h"
+/*
+ Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+ 
+ Note:
+ Elements in a subset must be in non-descending order.
+ The solution set must not contain duplicate subsets.
+ For example,
+ If nums = [1,2,2], a solution is:
+ 
+ [
+ [2],
+ [1],
+ [1,2,2],
+ [2,2],
+ [1,2],
+ []
+ ]
+*/
+namespace Subset2{
+    class Solution {
+    public:
+        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+            vector<vector<int>> ans;
+            return ans;
+        }
+    };
+}
