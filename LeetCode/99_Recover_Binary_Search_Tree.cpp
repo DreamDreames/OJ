f9ba564436e8d8a1e66f8ad5a4ed290058bcd60a
Rainbowbox <twentyfivehours@163.com>
2016-05-29 23:54:30 +0800

    AC 105 Construct Binary Tree From PreOrder And Inorder Traversal

--- LeetCode/99_Recover_Binary_Search_Tree.cpp
+++ LeetCode/99_Recover_Binary_Search_Tree.cpp
@@ -11,7 +11,34 @@ namespace RecoverBinarySearchTree{
     class Solution {
     public:
         void recoverTree(TreeNode* root) {
+            if(root == NULL)
+                return;
+            if(root->left != NULL && root->val < root->left->val){
+                binaryfy(root);
+            }
+        }
+    private:
+        TreeNode* binaryfy(TreeNode* root){
+            if(root == NULL)
+                return root;
+            TreeNode* left = root->left;
+            TreeNode* right = root->right;
+            if(left != NULL && root->val < left->val){
+                root->left = left->left;
+                root->right = left->right;
+                left->left = root;
+                left->right = right;
+                binaryfy(root);
+            }
+            else if(right != NULL && root->val > right->val){
+                root->left = right->left;
+                root->right = right->right;
+                right->left = left;
+                right->right  = root;
+                binaryfy(root);
+            }
             
+            return left;
         }
     };
 }
