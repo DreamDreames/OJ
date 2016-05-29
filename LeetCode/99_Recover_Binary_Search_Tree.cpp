f177a999c364ee9cdcfe91b1902f95d2f2766758
Rainbowbox <twentyfivehours@163.com>
2016-05-29 10:27:16 +0800

    Add 99 Recover Binary Search Tree

---
+++ LeetCode/99_Recover_Binary_Search_Tree.cpp
@@ -0,0 +1,17 @@
+#include "shared.h"
+/*
+ Two elements of a binary search tree (BST) are swapped by mistake.
+ 
+ Recover the tree without changing its structure.
+ 
+ Note:
+ A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+*/
+namespace RecoverBinarySearchTree{
+    class Solution {
+    public:
+        void recoverTree(TreeNode* root) {
+            
+        }
+    };
+}
