f025ddfa3d6a32c1281f832795d5b804d348396e
Rainbowbox <twentyfivehours@163.com>
2016-05-31 23:07:44 +0800

    AC 111 Minimum Depth of Binary Tree

---
+++ LeetCode/111_Minimum_Depth_Of_Binary_Tree.cpp
@@ -0,0 +1,18 @@
+#include "shared.h"
+
+namespace MinimumDepthOfBinaryTree {
+    class Solution {
+    public:
+        int minDepth(TreeNode* root) {
+            if(root == NULL)
+                return 0;
+            if(root->left == NULL && root->right == NULL)
+                return 1;
+            if(root->left == NULL)
+                return minDepth(root->right) + 1;
+            if(root->right == NULL)
+                return minDepth(root->left) + 1;
+            return min(minDepth(root->right), minDepth(root->left)) + 1;
+        }
+    };
+}
