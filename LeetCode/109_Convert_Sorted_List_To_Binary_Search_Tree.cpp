2b18f3bd25f48963f08ab2efc29ff583b4bf0161
Rainbowbox <twentyfivehours@163.com>
2016-05-31 22:14:06 +0800

    AC 109 Convert Sorted List to Binary Search Tree

---
+++ LeetCode/109_Convert_Sorted_List_To_Binary_Search_Tree.cpp
@@ -0,0 +1,28 @@
+#include "shared.h"
+/*
+ Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+namespace ConvertSortedListToBinarySearchTree{
+    class Solution {
+    public:
+        TreeNode* sortedListToBST(ListNode* head) {
+            if(head == NULL)
+                return NULL;
+            ListNode* slow = head, *fast = head, *pre = NULL;
+            while(fast != NULL && fast->next != NULL){
+                pre = slow;
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            if(pre != NULL)
+                pre->next = NULL;
+            TreeNode* root = new TreeNode(slow->val);
+            if(slow == head)
+                root->left = NULL;
+            else
+                root->left = sortedListToBST(head);
+            root->right = sortedListToBST(slow->next);
+            return root;
+        }
+    };
+}
