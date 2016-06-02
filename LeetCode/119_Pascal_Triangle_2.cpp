343303487f77071a484b35f99e676e73827bc486
Rainbowbox <twentyfivehours@163.com>
2016-06-03 00:03:03 +0800

    Cleanup code

--- LeetCode/119_Pascal_Triangle_2.cpp
+++ LeetCode/119_Pascal_Triangle_2.cpp
@@ -4,14 +4,12 @@ namespace PascalTriangle2 {
     class Solution {
     public:
         vector<int> getRow(int rowIndex) {
-            vector<int> ans (rowIndex + 1, 0);
+            vector<int> ans (rowIndex + 1, 1);
             int pre = 1;
             for(int i = 0; i <= rowIndex; ++ i){
                 for(int j = 0; j <= i; ++ j){
-                    if(j == 0 || j == i){
+                    if(j == 0 || j == i)
                         ans[j] = 1;
-                        pre = 1;
-                    }
                     else{
                         int temp = ans[j] + pre;
                         pre = ans[j];
