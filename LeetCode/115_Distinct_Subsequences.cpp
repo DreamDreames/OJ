c40560521ce87e6967d53aa31c9a734e36fa4859
Rainbowbox <twentyfivehours@163.com>
2016-06-04 20:45:08 +0800

    AC 115 Distinct Subsequences

--- LeetCode/115_Distinct_Subsequences.cpp
+++ LeetCode/115_Distinct_Subsequences.cpp
@@ -1,4 +1,4 @@
-#include "shared.h"
+#include "stdafx.h"
 /*
  Given a string S and a string T, count the number of distinct subsequences of T in S.
  
@@ -10,10 +10,24 @@
  Return 3.
  */
 namespace DistinctSubsequences{
-    class Solution {
-    public:
-        int numDistinct(string s, string t) {
-            return 0;
-        }
-    };
+	class Solution {
+	public:
+		int numDistinct(string s, string t) {
+			if (s.length() == 0 || t.length() == 0)
+				return 0;
+			vector<vector<int>> record(t.length(), vector<int>(s.length(), 0));
+
+			if (s[0] == t[0])
+				record[0][0] = 1;
+			for (int i = 0; i < t.length(); ++i) {
+				for (int j = 1; j < s.length(); ++j) {
+					if (t[i] == s[j])
+						record[i][j] = (i > 0 ? record[i - 1][j - 1] : 1) + record[i][j - 1];
+					else
+						record[i][j] = record[i][j - 1];
+				}
+			}
+			return record[t.length() - 1][s.length() - 1];
+		}
+	};
 }
