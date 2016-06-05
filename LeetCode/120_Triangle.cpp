bed4e0cff6b482ea20fd81a3bc1006f2003070c8
Rainbowbox <twentyfivehours@163.com>
2016-06-06 00:29:11 +0800

    AC 120 Triangle

--- LeetCode/120_Triangle.cpp
+++ LeetCode/120_Triangle.cpp
@@ -18,7 +18,32 @@ namespace Triangle{
     class Solution {
     public:
         int minimumTotal(vector<vector<int>>& triangle) {
-            return 0;
+            int size = (int)triangle.size();
+            if(size == 0 || triangle[0].size() == 0)
+                return 0;
+            vector<int> record(size);
+            record[0] = triangle[0][0];
+            for(int i = 1; i < triangle.size(); ++ i){
+                int prev = INT_MAX;
+                for(int j = 0; j <= i; ++ j){
+                    int minValue = min(j - 1 >= 0 ? prev : record[j], j <= i - 1? record[j] : prev);
+                    prev = record[j];
+                    record[j] = minValue + triangle[i][j];
+                }
+            }
+            int min = record[0];
+            for(int i = 1; i < record.size(); ++ i){
+                if(record[i] < min)
+                    min = record[i];
+            }
+            return min;
+            
         }
     };
+    TEST(Triangle, minimumTotal){
+        Solution sln;
+        vector<vector<int>> triangle = {{2},{3,4},{6,5,7},{4,1,8,3}};
+        int actual = sln.minimumTotal(triangle);
+        ASSERT_EQ(11, actual);
+    }
 }
