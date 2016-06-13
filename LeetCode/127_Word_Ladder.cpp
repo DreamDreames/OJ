eea3726f1ec1ce368bd5922b2a4d65d92246108c
Rainbowbox <twentyfivehours@163.com>
2016-06-13 21:16:26 +0800

    AC 127 Word Ladder (BFS)

--- LeetCode/127_Word_Ladder.cpp
+++ LeetCode/127_Word_Ladder.cpp
@@ -1,5 +1,4 @@
-#include "stdafx.h"
-#include <queue>
+#include "shared.h"
 #include <list>
 /*
  Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
@@ -27,59 +26,44 @@ namespace WordLadder{
             if(canConcat(beginWord, endWord))
                 return 2;
             
-            int size = (int)wordList.size() + 2;
-            vector<vector<int>> matrix (size, vector<int>(size, -1));
-            wordList.insert(beginWord);
             wordList.insert(endWord);
-            int startPos = 0, endPos = 0;
-            auto iter1 = wordList.begin();
-            for(int i = 0; i < size; ++ i, ++ iter1){
-                if(*iter1 == beginWord)
-                    startPos = i;
-                else if(*iter1 == endWord)
-                    endPos = i;
-				auto iter2 = wordList.begin();
-                for(int j = 0; j < size; ++ j, ++ iter2){
-                    if(i == j)
-                        matrix[j][i] = matrix[i][j] = 0;
-                    else if(canConcat(*iter1, *iter2))
-                        matrix[j][i] = matrix[i][j] = 1;
-                }
-            }
+            queue<pair<string,int>> q;
             
-            unordered_set<int> traversed;
-            list<int> untraversed;
-            for(int i = 0; i < size; ++ i){ 
-				untraversed.push_back(i);
-            }
-            queue<int> q;
-            q.push(startPos);
+            q.push(make_pair(beginWord, 1));
+            
             while(!q.empty()){
-                int current = q.front();
+                auto current = q.front();
                 q.pop();
-                for(auto iter = untraversed.begin(); iter != untraversed.end(); ++ iter){
-                    int index = *iter;
-                    if(index == current || traversed.find(index) != traversed.end())
-                        continue;
-                    if(matrix[current][index] == 1){
-                        q.push(index);
-                        for(auto iter = traversed.begin(); iter != traversed.end(); ++ iter){
-                            int temp = *iter;
-                            matrix[temp][index] = matrix[temp][current] + 1;
-                        }
-						if (index == endPos){
-							return matrix[startPos][endPos];
-						}
-                    }
+                auto neighbors = findNeighbors(current.first, wordList);
+                
+                for(auto& w : neighbors){
+                    if(w == endWord)
+                        return current.second + 1;
+                    q.push(make_pair(w, current.second + 1));
                 }
-                traversed.insert(current);
-                untraversed.remove(current);
+                
             }
-            if(matrix[startPos][endPos] == -1)
-                return 0;
-            return matrix[startPos][endPos];
+            return 0;
         }
      private:
+        vector<string> findNeighbors(string s, unordered_set<string>& wordList){
+            vector<string> result;
+            for(int i = 0; i < s.length(); ++ i){
+                char ch = s[i];
+                for(int j = 0; j < 26; ++ j){
+                    char newch = 'a' + j;
+                    if(ch == newch)
+                        continue;
+                    s[i] = newch;
+                    if(wordList.find(s) != wordList.end()){
+                        wordList.erase(s);
+                        result.push_back(s);
+                    }
+                }
+                s[i] = ch;
+            }
+            return result;
+        }
         bool canConcat(const string& word1, const string& word2){
             int diff = 0;
             for(int i = 0; i < word1.length(); ++ i){
@@ -100,22 +84,10 @@ namespace WordLadder{
                 wordSet.insert(*iter);
             }
             int actual = sln.ladderLength(startWord, endWord, wordSet);
-            //ASSERT_EQ(expected, actual);
-			Assert::AreEqual(expected, actual);
+            ASSERT_EQ(expected, actual);
         }
     };
-	TEST_CLASS(ladderLength){
-	public:
-		TEST_METHOD(WordLadder){
-			helper  h;
-			h.test("hit", "abc", {}, 0);
-			h.test("hit", "hot", {}, 2);
-			h.test("hit", "dot", { "hot", "abc" }, 3);
-			h.test("hit", "cog", { "hot", "dot", "dog", "lot", "log" }, 5);
-			h.test("qa", "sq", { "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye" }, 5);
-		}
-	};
-	/*
+
     TEST(WordLadder, ladderLength){
         helper  h;
         h.test("hit", "abc", {}, 0);
@@ -125,5 +97,4 @@ namespace WordLadder{
         h.test("qa", "sq", {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye" }, 5);
 
     }
-	*/
 }
