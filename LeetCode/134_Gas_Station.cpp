6ba9889d75ae0a3b96c140028cf891be632e84a8
Rainbowbox <twentyfivehours@163.com>
2016-06-14 23:46:53 +0800

    AC 134 Gas Station

--- LeetCode/134_Gas_Station.cpp
+++ LeetCode/134_Gas_Station.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "shared.h"
 /*
 There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
 
@@ -10,26 +10,57 @@ Note:
 The solution is guaranteed to be unique.
 */
 namespace GasStation{
-	class Solution {
-	public:
-		int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-			for (int i = 0; i < gas.size(); ++i){
-				if (canReach(gas, cost, i, i, 0))
-					return 1;
-			}
-			return -1;
-		}
-	private:
-		bool canReach(vector<int>& gas, vector<int>& cost, int startIndex, int endIndex, int currentGas){
-			currentGas = currentGas + gas[startIndex] - cost[startIndex];
-			if (currentGas < 0)
-				return false;
-			
-			startIndex = (startIndex + 1) % gas.size();
-			if (startIndex == endIndex)
-				return true;
-
-			return canReach(gas, cost, startIndex, endIndex, currentGas);
-		}
-	};
+    class Solution {
+    public:
+        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+            unordered_map<int, int> record;
+            for (int i = 0; i < gas.size(); ++i){
+                if (canReach(gas, cost, i, i, 0, record))
+                    return i;
+            }
+            return -1;
+        }
+    private:
+        bool canReach(vector<int>& gas, vector<int>& cost, int startIndex, int endIndex, int currentGas, unordered_map<int, int>& record){
+            if(record.find(startIndex) != record.end()){
+                if(record[startIndex] > currentGas)
+                    return false;
+            }
+            int tempGas = currentGas;
+            int tempIndex = startIndex;
+            
+            currentGas = currentGas + gas[startIndex] - cost[startIndex];
+            if (currentGas < 0){
+                return false;
+            }
+            
+            startIndex = (startIndex + 1) % gas.size();
+            if (startIndex == endIndex)
+                return true;
+            
+            if(!canReach(gas, cost, startIndex, endIndex, currentGas, record)){
+                if(record.find(tempIndex) != record.end()){
+                    if(tempGas > record[tempIndex])
+                        record[tempIndex] = tempGas;
+                }
+                else
+                    record[tempIndex] = tempGas;
+                return false;
+            }
+            return true;
+        }
+        
+    };
+    class helper{
+    public:
+        void test(vector<int> gas, vector<int> cost, int expected){
+            Solution sln;
+            int actual = sln.canCompleteCircuit(gas, cost);
+            ASSERT_EQ(expected, actual);
+        }
+    };
+    TEST(GasStation, canCompleteCircuit){
+        helper h;
+        h.test({5}, {4}, 0);
+    }
 }
