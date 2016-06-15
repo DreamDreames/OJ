6f1b34eb6fc942861633ad40915ab417230ff65a
Rainbowbox <twentyfivehours@163.com>
2016-06-15 17:34:03 +0800

    AC 137 Single Number 2

--- LeetCode/137_Single_Number_2.cpp
+++ LeetCode/137_Single_Number_2.cpp
@@ -9,7 +9,33 @@ namespace SingleNumber2{
     class Solution {
     public:
         int singleNumber(vector<int>& nums) {
-            return 0;
+            vector<int> bits(32, 0);
+            for(auto n : nums){
+                for(int i = 0; i < 32; ++ i){
+                    bits[i] += n & 1;
+                    n >>= 1;
+                    if(n == 0) break;
+                }
+            }
+            
+            int number = 0;
+            for(int i = 0; i < 32; ++ i){
+                number += (bits[i] % 3) << i;
+            }
+            return number;
         }
     };
+    
+    class helper{
+    public:
+        void test(vector<int> nums, int expected){
+            Solution sln;
+            int actual = sln.singleNumber(nums);
+            ASSERT_EQ(expected, actual);
+        }
+    };
+    TEST(SingleNumber2, singleNumber){
+        helper h;
+        h.test({-2,-2,1,1,-3,1,-3,-3,-4,-2}, -4);
+    }
 }
