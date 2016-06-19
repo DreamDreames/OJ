59697c3089c24d1d5afe1da626ae63c73760c612
Rainbowbox <twentyfivehours@163.com>
2016-06-19 23:37:25 +0800

    AC 162 Find Peak Element

---
+++ LeetCode/162_Find_Peak_Element.cpp
@@ -0,0 +1,61 @@
+#include "shared.h"
+/*
+ A peak element is an element that is greater than its neighbors.
+ 
+ Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+ 
+ The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ 
+ You may imagine that num[-1] = num[n] = -∞.
+ 
+ For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+ 
+ click to show spoilers.
+ 
+ Note:
+ Your solution should be in logarithmic complexity.
+*/
+namespace FindPeakElement{
+    class Solution {
+    public:
+        int findPeakElement(vector<int>& nums) {
+            int size = (int)nums.size();
+            int low = 0, high = size - 1;
+            while(low <= high){
+                if(low == high)
+                    return low;
+                int middle = (low + high) / 2;
+                if(middle == 0){
+                    if(nums[middle] > nums[middle + 1])
+                        return middle;
+                    low = middle + 1;
+                }
+                else if (middle == size - 1){
+                    if(nums[middle] > nums[middle - 1])
+                        return middle;
+                    high = middle - 1;
+                }
+                else{
+                    if(nums[middle] > nums[middle - 1] && nums[middle] > nums[middle + 1])
+                        return middle;
+                    if(nums[middle - 1] > nums[middle])
+                        high = middle - 1;
+                    else low = middle + 1;
+                }
+            }
+            return -1;
+        }
+    };
+    class helper{
+    public:
+        void test(vector<int> nums, int expected){
+            Solution sln;
+            int actual = sln.findPeakElement(nums);
+            ASSERT_EQ(expected, actual);
+        }
+    };
+    TEST(FindPeakElement, findPeakElement){
+        helper h;
+        h.test({2, 1}, 0);
+    }
+}
