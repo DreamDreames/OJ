957fbd721759d194304f22f3dbefd8bbb170e95c
Rainbowbox <twentyfivehours@163.com>
2016-06-29 08:45:09 +0800

    Add 124

---
+++ LeetCode/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,23 @@
+#include "shared.h"
+/*
+ Given a binary tree, find the maximum path sum.
+ 
+ For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path does not need to go through the root.
+ 
+ For example:
+ Given the below binary tree,
+ 
+  1
+ / \
+2   3
+ 
+Return 6.
+*/
+namespace BinaryTreeMaximumPathSum {
+    class Solution {
+    public:
+        int maxPathSum(TreeNode* root) {
+            return 0;
+        }
+    };
+}
